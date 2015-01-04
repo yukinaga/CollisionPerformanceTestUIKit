c13dab1c92b7dfdeaa120e3fd7dcbbf9c2473b61
yuky_az <yuky.azuma@gmail.com>
2015-01-04 14:19:35 +0900

    Initial Commit

---
+++ CollisionPerformanceTestUIKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollisionPerformanceTestUIKit
+//
+//  Created by Yukinaga Azuma on 2015/01/04.
+//  Copyright (c) 2015年 Yukinaga Azuma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CollisionPerformanceTestUIKit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CollisionPerformanceTestUIKit
+//
+//  Created by Yukinaga Azuma on 2015/01/04.
+//  Copyright (c) 2015年 Yukinaga Azuma. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
